dd1b9b7e1fdeb7c6fa4f2489fbf903ec693158e5
victorsilvaMR3 <victorsilva43@gmail.com>
2025-07-15 17:38:34 -0300

    fix: prisma client import e build compatível com produção

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,5 +1,5 @@
 import { Pool } from 'pg'
-import { PrismaClient } from '../generated/prisma'
+import { PrismaClient } from '@prisma/client'
 
 // Variável global para o pool
 let pool: Pool | null = null
